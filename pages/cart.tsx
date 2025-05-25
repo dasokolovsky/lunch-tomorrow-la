6cbb86804113bc8a2dccd7bce688cdf18770b4ef
Daniel Sokolovsky <d@sokolovsky.com>
2025-05-25 01:45:14 -0700

    Fix TypeScript error: Remove invalid paymentMethodTypes property
    - Removed paymentMethodTypes from PaymentElement options (not a valid property)
    - Payment method filtering is now handled by the backend payment_method_types
    - This should resolve the Vercel build error

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -140,9 +140,7 @@ function PaymentFormContent({
               wallets: {
                 applePay: 'auto',
                 googlePay: 'auto'
-              },
-              // Explicitly disable unwanted payment methods
-              paymentMethodTypes: ['card', 'apple_pay', 'google_pay']
+              }
             }}
             onReady={() => setPaymentElementReady(true)}
             onLoadError={(error) => {
